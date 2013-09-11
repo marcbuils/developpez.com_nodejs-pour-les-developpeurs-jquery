bdd526a5543fac4590e55fa0490224e432c4c00c
Marc Buils <marc.buils@marcbuils.fr>
2013-09-11 15:42:36 +0200

    Correction de l'indentation

--- src/jquery.httpserver.js
+++ src/jquery.httpserver.js
@@ -9,28 +9,29 @@
 				}
 			}, this );
 			
-			// CrŽation du serveur HTTP
-			var _serveur = http.createServer( function (req, res) { // fonction de callback appelŽe pour chaque appel de page
-				var _pathname = url.parse(req.url).pathname;
-				var _ext = _pathname.split('/');
+			// Crï¿½ation du serveur HTTP
+			var _serveur = http.createServer( function (req, res) { // fonction de callback appelï¿½e pour chaque appel de page
+				var 	_pathname = url.parse(req.url).pathname,
+					_ext = _pathname.split('/');
+					
 				_ext = _ext[_ext.length-1].split('.');
 				_ext = ( _ext.length < 2 ? '' : _ext[_ext.length-1] );
 				
 				fs.readFile( _options.path + _pathname, function(err, data){
-						if ( err ){
-							// code de statut 404 (FILE_NOT_FOUND) et retourne un fichier de type 'text/plain'
-							res.writeHead(404, {'Content-Type': 'text/plain'});
-							// Affiche une erreur
-							res.end('Ereur 404: Fichier non trouve\n');
-						} else {
-							// code de statut 200 (OK) et retourne un fichier de type 'text/plain'
-							res.writeHead(200, {'Content-Type': typeof(_options.types[ _ext ]) != "undefined" ? _options.types[ _ext ] : 'text/plain' });
-							// retourne le fichier trouve
-							res.end( data );
-						}
-					});
-				} )
-				.listen( _options.port, '127.0.0.1' );	// Le serveur Žcoutera le port 8090 
+					if ( err ){
+						// code de statut 404 (FILE_NOT_FOUND) et retourne un fichier de type 'text/plain'
+						res.writeHead(404, {'Content-Type': 'text/plain'});
+						// Affiche une erreur
+						res.end('Ereur 404: Fichier non trouve\n');
+					} else {
+						// code de statut 200 (OK) et retourne un fichier de type 'text/plain'
+						res.writeHead(200, {'Content-Type': typeof(_options.types[ _ext ]) != "undefined" ? _options.types[ _ext ] : 'text/plain' });
+						// retourne le fichier trouve
+						res.end( data );
+					}
+				});
+			} )
+			.listen( _options.port, '127.0.0.1' );	// Le serveur ï¿½coutera le port 8090 
 			
 			// Partage le serveur
 			$(this).data( 'httpserver', _serveur );
